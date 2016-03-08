4bc689d50389e5c7745c610344d735c8d30287b8
Luka Draksler <ld8141@student.uni-lj.si>
2016-03-08 15:35:55 +0000

    stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		document.querySelector("#opomniki").innerHTML += "\
-		<div class= 'opomnik'> \
+		<div class= 'opomnik senca rob'> \
         <div class='naziv_opomnika'>"+naziv_opomnik+"</div> \
         <div class='cas_opomnika'> Opomnik čez <span>"+cas_opomnika+"</span> sekund.</div>\
          </div>";
@@ -21,6 +21,9 @@ window.addEventListener('load', function() {
 	
 		document.querySelector("#dodajGumb").addEventListener('click',dodajOpomnik);
 	//Posodobi opomnike
+	var posodobiOpomnike = function(event){
+	
+	}
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
@@ -28,7 +31,14 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
-	
+		    if(cas == 0){
+		    	var naziv_opomnika = document.querySelector(".naziv_opomnika").innerHTML;
+		    	alert("opomnik\n\nZadolžitev '"+naziv_opomnika +"' je potekla!");
+		    	document.querySelector("#opomniki").removeChild(opomnik);
+		    }
+		    else{
+		    	casovnik.innerHTML = cas-1;
+		    }
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
